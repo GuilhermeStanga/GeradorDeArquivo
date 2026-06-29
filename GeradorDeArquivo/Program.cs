b884d4332df6bda73e39bc7aa4eeffa4f3687d3f
Guilherme Stanga <guistanga@gmail.com>
2026-06-29 13:40:27 -0300

    Initial Commit

---
+++ GeradorDeArquivo/Program.cs
@@ -0,0 +1,17 @@
+﻿using GeradorTxt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorDeArquivo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MainConsole.Run();
+        }
+    }
+}
